e4fa8d1804887d84676318a763dace89f327ca6f
Seif Zein <sn1279@fayoum.edu.eg>
2023-04-09 14:34:28 +0200

    Ass 3

---
+++ Flutter 3/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:ass_a_3/business_s_card.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home:  BusinessCardAppPage()
+    );
+  }
+}
+
+
